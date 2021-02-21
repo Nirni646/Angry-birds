0985111b913ed0dd93e0a348e763003653c388b4
anatshah02 <77918008+anatshah02@users.noreply.github.com>
2021-02-21 16:49:52 +0530

    Add files via upload

---
+++ pig.js
@@ -0,0 +1,26 @@
+class Pig {
+    constructor(x, y) {
+      var options = {
+          'restitution':0.8,
+          'friction':1,
+          'density':1.0
+      }
+      this.body = Bodies.rectangle(x, y, 80, 80, options);
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      strokeWeight(7)
+      stroke("yellow")
+      fill("green");
+      rect(0, 0, 50, 50);
+      pop();
+    }
+  };
+  
